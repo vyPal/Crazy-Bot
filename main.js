9a99d48ad63bacb5671cec6ff05659d1d0531a8e
vyPal <66716025+vyPal@users.noreply.github.com>
2021-03-09 08:15:27 +0100

    Added song commands and listeners from distube

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
+const DisTube = require('distube');
 const client = new Discord.Client();
 const prefix = '>';
 const fs = require('fs');
 client.commands = new Discord.Collection();
+const distube = new DisTube(client, { searchSongs: true, emitNewSongOnly: true });
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
@@ -63,7 +65,42 @@ client.on('message', message =>{
         client.commands.get('mute').execute(message, args)
     }else if (command == 'unmute'){
         client.commands.get('unmute').execute(message, args)
+    }else if (command == 'play'){
+        distube.play(message, args.join(' '));
+    }else if (command == "stop"){
+        distube.stop(message);
+    }else if (command == "skip"){
+        distube.skip(message);
+    }else if (command == "queue") {
+        let queue = distube.getQueue(message);
+        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
+            `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
+        ).slice(0, 10).join("\n"));
     }
 })
+
+const status = (queue) => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filter || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+distube
+    .on("playSong", (message, queue, song) => message.channel.send(
+        `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+    ))
+    .on("addSong", (message, queue, song) => message.channel.send(
+        `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    ))
+    .on("playList", (message, queue, playlist, song) => message.channel.send(
+        `Play \`${playlist.name}\` playlist (${playlist.songs.length} songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}`
+    ))
+    .on("addList", (message, queue, playlist) => message.channel.send(
+        `Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${status(queue)}`
+    ))
+    .on("searchResult", (message, result) => {
+        let i = 0;
+        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`);
+    })
+    .on("searchCancel", (message) => message.channel.send(`Searching canceled`))
+    .on("error", (message, e) => {
+        console.error(e)
+        message.channel.send("An error encountered: " + e);
+    });
 
 client.login('Your Token');
